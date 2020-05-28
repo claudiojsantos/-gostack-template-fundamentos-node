977312d7fd63bcf731ee30c5face3c3bf6648f9c
Cláudio Santos <cljos@hotmail.com>
2020-05-28 00:00:07 -0300

    Conclusão do desafio 05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const valorInicial = 0;
+    let income = 0;
+    let outcome = 0;
+
+    this.transactions.reduce(function (trs, tr) {
+      if (tr.type === 'income') {
+        income += tr.value;
+      } else {
+        outcome += tr.value;
+      }
+
+      return trs;
+    }, valorInicial);
+
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(
+    title: string,
+    value: number,
+    type: 'income' | 'outcome',
+  ): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
